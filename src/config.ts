8c7eaff22bf79b7312345513c28b26c619efd6e7
BL-sy <3026709446@qq.com>
2025-10-12 22:00:17 +0800

    更改配置

--- src/config.ts
+++ src/config.ts
@@ -33,11 +33,11 @@ export const siteConfig: SiteConfig = {
 
 	
 	bangumi: {
-		userId: "sai", // 在此处设置你的Bangumi用户ID，可以设置为 "sai" 测试
+		userId: "blsy", // 在此处设置你的Bangumi用户ID，可以设置为 "sai" 测试
 	},
 
 	anime: {
-		mode: "local", // 番剧页面模式："bangumi" 使用Bangumi API，"local" 使用本地配置
+		mode: "bangumi", // 番剧页面模式："bangumi" 使用Bangumi API，"local" 使用本地配置
 	},
 
 	banner: {
