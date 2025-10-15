d9b878e0ba4fcfaf7e154e1c6fb338b872666e69
BL-sy <3026709446@qq.com>
2025-10-15 17:14:55 +0800

    rename

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://BL-sy.github.io/",
+	site: "https://BLsy.github.io/",
 
 	base: "/",
 	trailingSlash: "always",
